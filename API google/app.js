7c89cf3fa6600c1f865f6cf671c5cb5398fcfde4
arthurpieri <arthurpieri@gitlab.com>
2017-12-15 02:14:24 -0200

    Criacao de arquivo weather.js e chamadas de api pelo Clima

--- API google/app.js
+++ API google/app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs')
 const geocode = require('./geocode/geocode.js')
+const weather = require('./weather/weather.js')
 
 const argv = yargs
     .options({
@@ -20,6 +21,14 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log (errorMessage)
     } else {
-        console.log(JSON.stringify(results, undefined, 2))
+        console.log(results.address)
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log (errorMessage)
+            } else {
+                console.log(`It's currently ${weatherResults.temperature}. But it feels like ${weatherResults.apparentTemperature}.`)
+            }
+        }) 
     }
 })
+
---
+++ API google/weather/weather.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+let getWeather = (lat, lon, callback) => {
+    let apiKey = 'b20d28c40c7b2ac92a594c93f9dee631'
+    request({
+        url: `https://api.darksky.net/forecast/${apiKey}/${lat},${lon}`,
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature                
+            })
+        } else {
+            callback('Unable to fetch weather.')
+        }
+    }
+)}
+
+module.exports.getWeather = getWeather
