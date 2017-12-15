137e16ce67412f556bae3c326925256172ab918a
arthurpieri <arthurpieri@gitlab.com>
2017-12-15 00:38:15 -0200

    Tratamento de erros

--- API google/app.js
+++ API google/app.js
@@ -20,7 +20,13 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}`,
     json: true
 }, (error, response, body) => {
+    if (error){
+        console.log('Impossivel conectar ao servidor do google')
+    } else if (body.status === 'ZERO_RESULTS'){
+        console.log ('Endereco nao encontrado')
+    } else if (body.status === 'OK'){
     console.log('Address: ' + body.results[0].formatted_address)
     console.log('Latitude: ', body.results[0].geometry.location.lat)
     console.log('Longitude: ', body.results[0].geometry.location.lng)
+    }
 })
