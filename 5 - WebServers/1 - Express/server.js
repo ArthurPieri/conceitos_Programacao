83a0028697e0156acb294e622c4d07fe2724631d
arthurpieri <arthurpieri@gitlab.com>
2017-12-18 14:18:16 -0200

    Aprendendo a mexer com Handlebars

--- 5 - WebServers/1 - Express/server.js
+++ 5 - WebServers/1 - Express/server.js
@@ -1,22 +1,30 @@
 const express = require('express')
+const handlebar = require('hbs')
 
 let app = express()
 
+let partials = hbs.registerPartials(__dirname + '/views/partials')
+let viewEngine = app.set('view engine', 'hbs')
 let public = app.use(express.static(__dirname + '/public'))
 
 let home = app.get('/', (request, response) => {
-//    response.send('<h1>Hello Express!</h1>')
-    response.send({
-        name: 'Arthur',
+    //    response.send('<h1>Hello Express!</h1>')
+    response.render('home.hbs', {
+        pageTitle: 'arthurpieri.com',
+        welcomeMessage: 'Bem vindo a ArthurPieri.com',
         likes: [
             'Cinema',
             'Videogames'
-        ]
+        ],
+        currentYear: new Date().getFullYear()
     })
 })
 
 let about = app.get('/about', (request, response) => {
-    response.send('About Page')
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
 })
 
 let bad = app.get('/bad',(request, response) => {
