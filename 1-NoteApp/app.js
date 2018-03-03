40776acfef94a544b8cbdca3272eff05ac87ffa5
Arthur Pieri <arthurpieri@gmail.com>
2018-03-03 20:15:42 -0300

    Learning how to use require, FS and OS modules

---
+++ 1-NoteApp/app.js
@@ -0,0 +1,14 @@
+console.log('Starting app.')
+
+const fs = require('fs')
+const os = require('os')
+
+let user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! \n`, (err) => {
+    if (err) {
+        console.log('Unable to write to file')
+    }
+})
+
+console.log('Finishing app.')
