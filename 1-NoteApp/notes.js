3df176eafeadb6042a52a7ba4b749f3294ca5d40
Arthur Pieri <arthurpieri@gmail.com>
2018-03-03 22:35:12 -0300

    Filling the notes.js file to really save the notes to an archive

--- 1-NoteApp/notes.js
+++ 1-NoteApp/notes.js
@@ -1,7 +1,31 @@
 // Notes.js archive
 
+const fs = require('fs')
+
 let addNote = (title, body) => {
-    console.log('Adding note', title, body)
+    let notes = []
+    let note = {
+        title,
+        body
+    }
+
+    try {
+        let notesString = fs.readFileSync('notes-data.json')
+        notes = JSON.parse(notesString)    
+    } catch (e) {
+        console.log('File does not exist')
+        console.log('Creating File')
+    }
+
+    let duplicateNotes = notes.filter((note)=> note.title === title)
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note)
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes))    
+    } else {
+        console.log('The tittle you provide is already in use')
+    }
+
 }
 
 let getAll = () => {
