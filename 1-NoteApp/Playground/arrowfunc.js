4f699be2dca4e674fd6ba790adce1ea74eff93f0
Arthur Pieri <arthurpieri@gmail.com>
2018-03-04 15:42:54 -0300

    A little bit more about Arrow Functions

---
+++ 1-NoteApp/Playground/arrowfunc.js
@@ -0,0 +1,15 @@
+let square = (x) => x * x
+
+console.log(square(9))
+
+let user = {
+    name: 'Arthur',
+    sayHi: () => {
+        console.log(`Hi.`)
+    },
+    sayHiAlt () {
+        console.log(`Hi. I'm ${this.name}`)
+    }
+}
+
+user.sayHiAlt()
