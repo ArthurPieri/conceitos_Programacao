65a9c5379a67ca8af3dd1495fd45585b9f0b8bea
Arthur Pieri <arthurpieri@gmail.com>
2018-03-03 21:39:08 -0300

    Requiring lodash and using npm init and npm install

--- 1-NoteApp/app.js
+++ 1-NoteApp/app.js
@@ -2,11 +2,18 @@ console.log('Starting app.js ')
 
 const fs = require('fs')
 const os = require('os')
+const _ = require('lodash')
 const notes = require('./notes.js')
 
-console.log ('Result: ', notes.add(8,2))
+/*console.log(_.isString(true))
+console.log(_.isString('Arthur')) */
 
-/*let user = os.userInfo();
+let filteredArray = _.uniq(['Arthur', 1, 'Arthur', 1, 2, 3, 4])
+console.log(filteredArray)
+
+/*console.log ('Result: ', notes.add(8,2))
+
+let user = os.userInfo();
 
 fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} \n`, (err) => {
     if (err) {
