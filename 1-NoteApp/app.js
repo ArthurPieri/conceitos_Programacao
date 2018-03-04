af8dce3959311e1d66096cc4278d94c1aeaec9f2
Arthur Pieri <arthurpieri@gmail.com>
2018-03-03 21:20:32 -0300

    Learning how to require own files and module.exports

--- 1-NoteApp/app.js
+++ 1-NoteApp/app.js
@@ -1,14 +1,17 @@
-console.log('Starting app.')
+console.log('Starting app.js ')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes.js')
 
-let user = os.userInfo();
+console.log ('Result: ', notes.add(8,2))
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! \n`, (err) => {
+/*let user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} \n`, (err) => {
     if (err) {
         console.log('Unable to write to file')
     }
 })
-
+*/
 console.log('Finishing app.')
