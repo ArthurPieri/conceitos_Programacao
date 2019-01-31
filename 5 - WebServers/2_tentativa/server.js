1357bff751c9987b0977a520599988debdc17b8f
arthur <arthurpieri@BSBDSKLNX06.castmeta.local>
2019-01-31 17:16:38 -0200

    Handlebars partials and helpers

--- 5 - WebServers/2_tentativa/server.js
+++ 5 - WebServers/2_tentativa/server.js
@@ -3,22 +3,30 @@ const hbs = require('hbs')
 
 let app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname+'/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello express!</h1> Its treason then')
     res.render('home.hbs', {
         pageTitle: 'Arthur Pieri',
-        currentYear: new Date().getFullYear(),
         welcomeMsg: 'Well, Hello there'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About page'
     })
 })
 
