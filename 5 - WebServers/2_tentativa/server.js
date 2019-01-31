8c85a89b368ca3c8dceb2d843f6c9b9bfee4bbf4
arthur <arthurpieri@BSBDSKLNX06.castmeta.local>
2019-01-31 17:01:28 -0200

    Começando a mexer com handlebars

--- 5 - WebServers/2_tentativa/server.js
+++ 5 - WebServers/2_tentativa/server.js
@@ -1,22 +1,25 @@
 const express = require('express')
+const hbs = require('hbs')
 
 let app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname+'/public'))
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello express!</h1> Its treason then')
-    res.send({
-        name: 'Arthur Pieri',
-        likes: [
-            'not life',
-            'to die'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Arthur Pieri',
+        currentYear: new Date().getFullYear(),
+        welcomeMsg: 'Well, Hello there'
     })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    })
 })
 
 app.get('/bad', (req, res) => {
