f813f60a13649b73ec629a3f7a09232f802d3786
Arthur Pieri <arthurpieri@gmail.com>
2019-03-25 15:17:02 -0300

    Creating endpoints and testing it with postman

--- nodev3/4-TaskManager/src/db/mongoose.js
+++ nodev3/4-TaskManager/src/db/mongoose.js
@@ -4,23 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
-
-const me = new User({
-    name: 'Arthur',
-    age: 26
-})
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error: ', error)
-})
