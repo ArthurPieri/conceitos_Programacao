1f19c35255586e942db3ef5205ece908d27b949f
Arthur Pieri <arthurpieri@gmail.com>
2019-04-15 12:04:14 -0300

    Adding jwt-tokens to login and create user

--- nodev3/4-TaskManager/src/routers/userRouter.js
+++ nodev3/4-TaskManager/src/routers/userRouter.js
@@ -2,21 +2,25 @@ const express = require('express')
 const User = require('../models/users')
 const router = new express.Router()
 
+// Public
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     }catch (e) {
         res.status(400).send(e)
     }
 })
 
+// Public
 router.post('/users/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     }catch(e){
         res.status(400).send()
     }
