690842b97ace95ed6da02be1d1a8923bb30428f7
Arthur Pieri <arthurpieri@gmail.com>
2019-04-18 18:29:53 -0300

    Finaly putting the filter method on

--- nodev3/4-TaskManager/src/routers/taskRouter.js
+++ nodev3/4-TaskManager/src/routers/taskRouter.js
@@ -19,9 +19,15 @@ router.post('/tasks', auth, async (req, res) => {
 // GET /tasks?completed=true or completed=false
 router.get('/tasks', auth, async (req, res) => {
     try{
-        // const tasks = await task.find({})
-        await req.user.populate('tasks').execPopulate()
-        res.send(req.user.tasks)
+        if (req.query.completed){
+            const tasks = await Task.find({ 
+                owner: req.user._id,
+                completed: req.query.completed
+            })    
+            return res.send(tasks)
+        } 
+        const tasks = await Task.find({ owner: req.user._id })
+        res.send(tasks)
     }catch(e){
         res.status(500).send()
     } 
