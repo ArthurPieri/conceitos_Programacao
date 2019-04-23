063ff87759a1faf92687ae60a076c5d7a747920d
Arthur Pieri <arthurpieri@gmail.com>
2019-04-23 19:25:37 -0300

    Adding test cases to user.test.js

--- nodev3/4-TaskManager/tests/user.test.js
+++ nodev3/4-TaskManager/tests/user.test.js
@@ -1,5 +1,18 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/users')
+
+const userOne = {
+    name: 'Jose',
+    email: 'jose@jose.com',
+    password: '123teste123'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -7,4 +20,18 @@ test('Should signup a new user', async () => {
         email: 'arthur@arthurpieri.com',
         password: 'MyPass123123'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login non existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'arthurpieri@jose.com',
+        password: 'blablabal123213'
+    }).expect(400)
 })
