58342bfe610366ec20c40873374f121a30bdbfa5
Arthur Pieri <arthurpieri@gmail.com>
2019-06-24 17:33:41 -0300

    Starting the basics course

---
+++ basics/temp-conversion.js
@@ -0,0 +1,12 @@
+let fahrenheit = 50
+
+// Calculate celsius and store in celsius variable
+// print that value
+let celsius = (fahrenheit - 32) * ( 5 / 9 )
+
+// calculate kelvin and store in kelvin variable
+// print that value
+let kelvin = (fahrenheit + 459.67) * ( 5 / 9 )
+
+console.log('Celsius: ',celsius)
+console.log('Kelvin: ',kelvin)
