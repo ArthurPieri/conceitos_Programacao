597299ff379df1ad37d04a012cac4e355ce80505
Arthur Pieri <git@arthurpieri.com>
2020-09-14 23:04:03 -0300

    Python

--- anhanguera/lingProg/aula1.py
+++ anhanguera/lingProg/aula1.py
@@ -1,1 +1,17 @@
-print("Hello world")
+salario = int(input("Salario: "))
+imposto = input("Imposto (Que eh roubo): ")
+
+if not imposto:
+    imposto = 27.5
+else:
+    imposto = float(imposto)
+
+sal = salario - (salario * (imposto*0.01))
+
+def jose():{
+    print("testado")
+}
+
+jose()
+
+print(sal)
