d344a406419c4489095f83cb766e91b2920a8f85
Arthur Pieri <git@arthurpieri.com>
2021-11-03 23:40:53 -0300

    Finalizando a parte 2 do tutorial

--- nestjs-pgsql-api/src/app.module.ts
+++ nestjs-pgsql-api/src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
