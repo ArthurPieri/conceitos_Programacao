2e6eb2a2f4e5fdfc4d2c6bdb6cb481514f38a6b0
Arthur Pieri <git@arthurpieri.com>
2021-11-10 12:27:27 -0300

    starting section 5, next fix some of the DONT DO THIS on the app

--- 1-messages/src/messages/messages.controller.ts
+++ 1-messages/src/messages/messages.controller.ts
@@ -1,23 +1,44 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DONT DO THIS ON REAL APP
+    // USE DEPENDECY INJECTION
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    return `all messages`;
+    return this.messagesService.findAll();
   }
 
   @Post()
   // This maybe weird, but we will use the CreateMessageDto as a type
   // and Nest will do the magic of validating if the body has a "variable" with the type we defined
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
-    return `message by id`;
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
