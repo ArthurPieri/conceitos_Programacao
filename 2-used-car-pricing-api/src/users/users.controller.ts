60c53c33c14f57215d88b918c72f454f716e2162
Arthur Pieri <git@arthurpieri.com>
2021-11-18 23:07:07 -0300

    Using NestJs recommended solution to treat passwords on requests

--- 2-used-car-pricing-api/src/users/users.controller.ts
+++ 2-used-car-pricing-api/src/users/users.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Query,
   Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -22,6 +24,7 @@ export class UsersController {
   }
 
   // In the param everything comes as a string
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
