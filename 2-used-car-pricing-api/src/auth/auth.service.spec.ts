51c28becb21b39011e1fc22a99ca9de0e8daabfb
Arthur Pieri <git@arthurpieri.com>
2021-11-22 17:39:31 -0300

    Automatic Push from script

---
+++ 2-used-car-pricing-api/src/auth/auth.service.spec.ts
@@ -0,0 +1,90 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/users.entity';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUSersService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    const users: User[] = [];
+
+    fakeUSersService = {
+      find: (email) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUSersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('asd@asd.com', '123456');
+
+    expect(user.password).not.toEqual('123456');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs with email that is in use', async (done) => {
+    fakeUSersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: 'b' } as User]);
+    try {
+      await service.signup('asdf@asdf.com', 'asdf');
+    } catch (e) {
+      done();
+    }
+  });
+
+  it('throws if signin is called with an unused email', async (done) => {
+    try {
+      await service.signin('asdf@asdf.com', 'pasdff');
+    } catch (e) {
+      done();
+    }
+  });
+
+  it('throws if an invalid password is provided', async (done) => {
+    fakeUSersService.find = () =>
+      Promise.resolve([{ email: 'asdf@asdf.com', password: 'pasdff' } as User]);
+
+    try {
+      await service.signin('asdf@asdf.com', 'password');
+    } catch (e) {
+      done();
+    }
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    await service.signup('asdf@asdf.com', 'mypassword');
+
+    const user = await service.signin('asdf@asdf.com', 'mypassword');
+    expect(user).toBeDefined();
+  });
+});
