03a197a355f4c94897e535aefc6477661b7801a1
Arthur Pieri <git@arthurpieri.com>
2022-01-08 17:06:13 -0300

    Finishing challenge 2 and starting interpolation

--- 4_vue_js_udemy/1_vuejs_dom/desafio-02/app.js
+++ 4_vue_js_udemy/1_vuejs_dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        exibirAlerta() {
+            alert(`Estou te alertando`)
+          },
+        apresentarValor(e) {
+            this.valor = e.target.value
+        }
     }
 })
