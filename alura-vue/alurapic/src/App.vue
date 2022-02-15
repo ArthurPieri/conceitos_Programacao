caceeecab16b5016e1dc23edef2c6fe982fc0c48
Arthur Pieri <git@arthurpieri.com>
2022-02-15 01:08:00 -0300

    Finalizando sessão 6 do curso 86%

--- alura-vue/alurapic/src/App.vue
+++ alura-vue/alurapic/src/App.vue
@@ -1,87 +1,50 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <input
-      type="search"
-      class="filtro"
-      placeholder="Filtre por parte do título"
-      @input="filtro = $event.target.value"
-    />
+    <meu-menu :rotas="routes"/>
 
-    <ul class="lista-fotos">
-      <li
-        class="lista-fotos-item"
-        v-for="foto in fotosComFiltro"
-        :key="foto.id"
-      >
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
-        </meu-painel>
-      </li>
-    </ul>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from "./componentes/shared/painel/Painel.vue";
-import ImagemResponsiva from "./componentes/shared/imagem-reponsiva/ImagemResponsiva.vue";
+
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+
   components: {
-    "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
+    'meu-menu' : Menu
   },
+  
   data() {
+
     return {
-      titulo: "Alura Pic",
-      fotos: [],
-      filtro: ""
-    };
-  },
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
+
+      routes
     }
-  },
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(
-        fotos => (this.fotos = fotos),
-        err => console.log(err)
-      );
   }
-};
+}
 </script>
 
 <style>
-.corpo {
-  font-family: Helvetica, sans-serif;
-  width: 96%;
-  margin: 0 auto;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
-.centralizado {
-  text-align: center;
-}
+ .pagina-enter, .pagina-leave-active {
 
-.lista-fotos {
-  list-style: none;
-}
+     opacity: 0;
+ }
 
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
+ .pagina-enter-active, .pagina-leave-active {
 
-.filtro {
-  display: block;
-  width: 100%;
-}
+     transition: opacity .4s;
+ }
+
 </style>
