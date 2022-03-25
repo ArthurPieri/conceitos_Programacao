df5a79a37d351d544ec01e7b6425f96ef1f25e0f
Arthur Pieri <git@arthurpieri.com>
2022-03-25 10:59:10 -0300

    Automatic Push from script

--- alura-vue/alurapic/src/components/home/Home.vue
+++ alura-vue/alurapic/src/components/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="centralizado">{{ titulo }}</h1>
+    <h1 v-meu-transform class="centralizado">{{ titulo }}</h1>
 
     <input
       type="search"
@@ -12,7 +12,11 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto">
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <imagem-responsiva
+            v-meu-transform
+            :url="foto.url"
+            :titulo="foto.titulo"
+          />
           <meu-botao
             tipo="button"
             rotulo="REMOVER"
